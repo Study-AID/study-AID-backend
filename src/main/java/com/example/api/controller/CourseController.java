2bbe1abdbf1dc9e26919a335675250c5d7fa411d
mingrammer <mingrammer@gmail.com>
2025-04-22 00:17:56 +0900

    chore: remove unneccesary line

--- src/main/java/com/example/api/controller/CourseController.java
+++ src/main/java/com/example/api/controller/CourseController.java
@@ -44,7 +44,6 @@ public class CourseController {
                             name = "semesterId",
                             description = "ID of the semester",
                             required = true,
-                            in = ParameterIn.PATH
                     )
             },
             responses = {
