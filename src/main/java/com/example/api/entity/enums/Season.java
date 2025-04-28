f8c34f1a69882c1ab1c9cf3c0aba31ec67fe0b4a
mingrammer <mingrammer@gmail.com>
2025-04-28 21:18:10 +0900

    fix: add missing file

--- src/main/java/com/example/api/entity/enums/Season.java
+++ src/main/java/com/example/api/entity/enums/Season.java
@@ -1,5 +1,14 @@
 package com.example.api.entity.enums;
 
 public enum Season {
-    spring, summer, fall, winter
+    spring, summer, fall, winter;
+
+    public static Season fromString(String season) {
+        for (Season s : Season.values()) {
+            if (s.name().equalsIgnoreCase(season)) {
+                return s;
+            }
+        }
+        throw new IllegalArgumentException("Invalid season: " + season);
+    }
 }
