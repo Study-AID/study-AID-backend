06293b2d5ed0697a42e61cefdd1aeee6e16f02d4
HyunjinNoh <noh0605@ajou.ac.kr>
2025-05-22 03:53:14 +0900

    chore: add TODO

--- src/main/java/com/example/api/promptsupport/PromptPaths.java
+++ src/main/java/com/example/api/promptsupport/PromptPaths.java
@@ -2,6 +2,7 @@ package com.example.api.promptsupport;
 
 public class PromptPaths {
 
+    // TODO(jin): add version config
     public static final String QNA_ANSWER_V1 = "prompts/qna_answer/v1.yaml";
     public static final String QNA_RECOMMEND_QUESTIONS_V1 = "prompts/qna_recommend_questions/v1.yaml";
     private PromptPaths() {
