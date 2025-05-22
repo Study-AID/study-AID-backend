2caf784fffa5ae09df04aadb5b8179f9da3db96f
MinJae Kwon (Miti) <mingrammer@gmail.com>
2025-05-22 14:40:57 +0900

    fix(auth): type mismatch issue (#80)
    * fix(auth): unable to login
    
    * fix: cast String to UUID instead of direct use
    
    * fix: test

--- src/main/java/com/example/api/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/api/security/jwt/JwtAuthenticationFilter.java
@@ -10,11 +10,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.io.IOException;
 import java.util.List;
