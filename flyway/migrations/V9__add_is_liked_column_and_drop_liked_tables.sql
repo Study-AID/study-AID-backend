067d6d8e0747c3481ecfa3ae1e8d81048c059bf1
triplenieun <triplenieun@ajou.ac.kr>
2025-05-29 21:17:39 +0900

    update v9 sql

--- flyway/migrations/V9__add_is_liked_column_and_drop_liked_tables.sql
+++ flyway/migrations/V9__add_is_liked_column_and_drop_liked_tables.sql
@@ -4,5 +4,6 @@ ALTER TABLE app.exam_items
     ADD COLUMN is_liked BOOLEAN DEFAULT FALSE NOT NULL;
 ALTER TABLE app.qna_chat_messages
     ADD COLUMN is_liked BOOLEAN DEFAULT FALSE NOT NULL;
+DROP TABLE IF EXISTS app.liked_quiz_items;
 DROP TABLE IF EXISTS app.liked_exam_items;
 DROP TABLE IF EXISTS app.liked_qna_chat_messages;
